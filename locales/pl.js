abf2bca9fe6c7a9b5cd8f5b802f307f01dc1f9cc
Tadeusz Łazurski <tadeusz@lazurski.pl>
2014-07-01 19:27:08 +0200

    New strings translated.

--- locales/pl.js
+++ locales/pl.js
@@ -82,5 +82,7 @@
 	"Please tell us your story.": "Opowiedz nam o swoim przypadku.",
 	"Do it now!": "Pomóż nam!",
 	"%s applied a question reference to a story.": "%s przypisał pytanie do opisu przypadku.",
-	"%s applied a draft by %s to a story": "%s opublikował propozycję opisu przypadku, której autorem jest %s"
+	"%s applied a draft by %s to a story": "%s opublikował propozycję opisu przypadku, której autorem jest %s",
+	"Nothing like that found. Sorry :P": "Nie znalaziono nic podobnego. Przykro nam.",
+	"Not published yet": "Jeszcze nie opublikowano."
 }
